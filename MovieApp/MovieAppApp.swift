d4c60711224f418945d7d2d13bc079bf4c7c4e71
Aryan Jagarwal <ajbaggar@gmail.com>
2023-06-06 11:25:59 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Aryan Jagarwal on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
